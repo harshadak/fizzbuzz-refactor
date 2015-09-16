00e7790de6c7e6bfad3044b02462bbdc5a370beb
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2015-09-15 17:20:38 -0700

    Refactored the code. One minor thing to be fixed in the next commit - display error message when the user enters a decimal value.

--- js/app.js
+++ js/app.js
@@ -1,38 +1,43 @@
-function fizzBuzz() {
-	var num = $('#inputValue').val();
-	//var numList = $('.result .content');
+$(document).ready(function() {
+	var num = prompt('Please enter a number:');
+	if (num != null) {
+		fizzBuzz(num);
+	}
+	
+	$('#calculate').on('click', function() {
+		var num = $('#inputValue').val();
+		num = parseInt(num, 10);
+
+		if (num % 1 !== 0) {
+			alert('No decimal values please!!'); // Not working!!
+		} else if (num % 1 === 0 &&  num >= 1) {
+			fizzBuzz(num);
+		} else {
+			alert('You must enter an integer value!!');
+			$('#inputValue').val('');
+		}
+	});
+
+	
+	$('#clear').on('click',function() {
+		$('#list').empty();
+		$('#inputValue').val('');
+	});
+});
+
+
+function fizzBuzz(num) {
 	var numList = $('#list');
 
 	for (var i = 1; i <= num; i++) {
 		if (i % 15 == 0) {
 			numList.append('<li class="answer">' + "FizzBuzz" + '</li>');
-			//numList.append('<p>FizzBuzz</p>');
 		} else if (i % 5 == 0) {
 			numList.append('<li class="answer">' + "Buzz" + '</li>');
-			//numList.append('<p>Buzz</p>');
 		} else if (i % 3 == 0) {
 			numList.append('<li class="answer">' + "Fizz" + '</li>');
-			//numList.append('<p>Fizz</p>');
 		} else {
 			numList.append('<li class="answer">' + i + '</li>');
-			//numList.append(i);
 		}
 	}
-
-	//$('#inputValue').val('');
-	//$('#list').('');
 }
-
-function clearList() {
-	$('#list').empty();
-	$('#inputValue').val('');
-}
-
-
-
-
-$(function() {
-	$('#calculate').on('click', fizzBuzz);
-	$('#clear').on('click', clearList);
-	//numList.empty();
-});
